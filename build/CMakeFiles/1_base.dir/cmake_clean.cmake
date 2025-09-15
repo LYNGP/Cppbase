6eb34d92ed958eadb9cce8f13c1637663c5766ad
QWEADDGFG <231060023@hdu.edu.cn>
2025-09-15 15:49:16 +0800

    update 20250915 15:49:16

---
+++ build/CMakeFiles/1_base.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/1_base"
+  "../bin/1_base.pdb"
+  "CMakeFiles/1_base.dir/src/1_base.cpp.o"
+  "CMakeFiles/1_base.dir/src/1_base.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_base.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ include/cplusplus.hpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <fmt/format.h>
+#include <fmt/chrono.h>
+#include <chrono>
+#include <string>
