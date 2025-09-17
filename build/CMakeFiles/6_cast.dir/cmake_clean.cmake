3d0b748c1bcaf3fe990b43b137bba311509d77e1
QWEADDGFG <231060023@hdu.edu.cn>
2025-09-17 10:01:36 +0800

    update 20250917 10:01:36

---
+++ build/CMakeFiles/6_cast.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/6_cast"
+  "../bin/6_cast.pdb"
+  "CMakeFiles/6_cast.dir/src/6_cast.cc.o"
+  "CMakeFiles/6_cast.dir/src/6_cast.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/6_cast.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
